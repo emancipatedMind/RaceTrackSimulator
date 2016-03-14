8dfbdeb582d86aa2fc3226d1eee76d51a868eb6f
emancipatedMind <powensfinch@gmail.com>
2016-03-14 01:15:02 -0400

    Updated skeleton
    Added classes, and information given about classes. Also, added images.

---
+++ Greyhound.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RaceTrack_Simulator {
+    class Greyhound {
+        public int StartingPosition; // Where myPictureBox starts.
+        public int RacetrackLength; // How long the racetrack is.
+        public PictureBox MyPictureBox; // PictureBox object.
+        public int Location = 0; // My location on the racetrack.
+        public Random Randomizer; // An instance of Random.
+
+        public bool Run() {
+            // Move forward either 1, 2, 3, or 4 spaces at random.
+            // Update the position of myPictureBox on the form like this:
+            //    MyPictureBox.Left = StartingPosition + Location;
+            // Return true if I won the race.
+        }
+
+        public void TakeStartingPosition() {
+            // Reset my location to 0, and myPictureBox to starting position.
+        }
+
+    }
+}
